c951feae7ad06f5237b8f2a61866a6bb1d44491b
zhangze-github <41512027+zhangze-github@users.noreply.github.com>
2018-09-09 21:47:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,84 @@
-var list = [{
-    title: "aaaa",
-    checked: false
-}, {
-    title: "bbbbb",
-    checked: false
-}]
-var vm = new Vue({
-    el: ".main",
+var setLocal = {
+    save(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    get(key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+}
+
+var list = setLocal.get('todos') || [];
+
+
+var App = new Vue({
+    el: '.main',
     data: {
-        list,
-        newTodo: "",
-        editingTodo: ""
+        list: list,
+        todo: '',
+        editingTodo: '',
+        beforeTodo: '',
+        visibility: 'all'
+    },
+    watch: {
+        list: {
+            handler: function () {
+                setLocal.save('todos', this.list);
+            },
+            deep: true
+        }
     },
     computed: {
-        count: function () {
-            return this.list.filter(function (item) {
-                return !item.checked
-            }).length
+        getChecked: function () {
+            return list.filter(function (todo) {
+                return !todo.checked;
+            }).length;
+        },
+        filterList: function () {
+            var filter = {
+                all: function (list) {
+                    return list;
+                },
+                unfinish: function (list) {
+                    return list.filter(function (item) {
+                        return !item.checked;
+                    });
+                },
+                finish: function (list) {
+                    return list.filter(function (item) {
+                        return item.checked;
+                    });
+                }
+            }
+            return filter[this.visibility] ? filter[this.visibility](this.list) :
+                this.list;
+        }
+    },
+    methods: {
+        addTodo: function () {
+            var value = this.todo;
+            if (value) {
+                this.list.push({
+                    title: value,
+                    checked: false
+                });
+            }
+            this.todo = '';
+        },
+        deleteTodo: function (todo) {
+            var index = this.list.indexOf(todo);
+            this.list.splice(index, 1);
+        },
+        editTodo: function (todo) {
+            this.editingTodo = todo;
+            this.beforeTodo = todo.title;
+        },
+        editedTodo: function () {
+            this.editingTodo = '';
+        },
+        cancelTodo: function (todo) {
+            todo.title = this.beforeTodo;
+            this.beforeTodo = '';
+            this.editingTodo = '';
         }
     },
     directives: {
@@ -27,23 +89,12 @@ var vm = new Vue({
                 }
             }
         }
-    },
-    methods: {
-        addTodo: function () {
-            this.list.push({
-                title: this.newTodo,
-                checked: false
-            })
-            this.newTodo = ""
-        },
-        editedTodo: function () {
-            this.editingTodo = ""
-        },
-        editTodo: function (todo) {
-            this.editingTodo = todo;
-        },
-        deleteTodo: function (index) {
-            this.list.splice(index, 1)
-        }
     }
-})
+});
+
+function hashchange() {
+    var hash = location.hash.substring(1);
+    App.visibility = hash;
+}
+hashchange();
+window.addEventListener('hashchange', hashchange);
